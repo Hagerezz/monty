c5f7fce6a8424414fb5ee12e95cc7f6342120ab8
Amna2017 <asiaashour2017@gmail.com>
2023-06-21 18:56:30 -0700

    pop amna

---
+++ pop.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+/**
+ * pop - pop to exit element from stack
+ * @stack : stack_t
+ * @line_number : number of lines in stacl
+ * Return : stack node and empty stck
+ *
+ */
+
+void pop(stack_t **stack, unsigned int line_number)
+{
+    if (!*stack)
+    {
+        dprintf("L%u: can't pop an empty stack\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+    stack_t *node = *stack;
+    *stack = node->next;
+    if (*stack)
+        (*stack)->prev = NULL;
+    free(node);
+}
