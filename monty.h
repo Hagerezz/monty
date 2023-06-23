166066d80a3e1688256de70597091e4943654394
Hagerezz <queen.emilly12@gmail.com>
2023-06-22 19:00:21 -0700

    a

--- monty.h
+++ monty.h
@@ -37,6 +37,7 @@ typedef struct instruction_s
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
@@ -45,6 +46,5 @@ void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 int execute(stack_t **stack, char *line, unsigned int count, FILE *fp);
-
 
 #endif
